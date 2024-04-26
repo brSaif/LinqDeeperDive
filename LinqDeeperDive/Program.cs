1c3a875a9945643667a05daa9fe79ba714630279
brSaif <saifeddine.benromdhane@protonmail.com>
2024-04-26 11:28:23 +0100

    Before optimization

--- LinqDeeperDive/Program.cs
+++ LinqDeeperDive/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
+using System.Diagnostics;
 using BenchmarkDotNet.Attributes;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using BenchmarkDotNet.Running;
 
-namespace LinqDeeperDive;
-
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 
 [MemoryDiagnoser]
 [ShortRunJob]
@@ -27,13 +27,20 @@ public class Tests
     [Benchmark]
     public int SumManual()
     {
-        return -1;
+        int sum = 0;
+        
+        foreach (int i in SelectManual(source, i => i + 2))
+        {
+            sum += i;
+        }
+
+        return sum;
     }
 
     [Benchmark]
     public int SumLinq()
     {
-        return -1;
+        return source.Sum(i => i + 2);
     }
    
     public static IEnumerable<TResult> SelectCompiler<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
@@ -51,5 +58,91 @@ public class Tests
             }
         }
     }
+
+    public static IEnumerable<TResult> SelectManual<TSource, TResult>(IEnumerable<TSource> source,
+        Func<TSource, TResult> selector)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(selector);
+
+        return new SelectManualEnumerable<TSource, TResult>(source, selector);
+    }
+}
+
+sealed class SelectManualEnumerable<TSource, TResult> : IEnumerable<TResult>, IEnumerator<TResult>
+{
+    private IEnumerable<TSource> _source;
+    private Func<TSource, TResult> _selector;
+
+    private int _threadId = Environment.CurrentManagedThreadId;
+    private TResult _current = default!;
+    private IEnumerator<TSource>? _enumerator;
+    private int _state = 0;
+    
+    public SelectManualEnumerable(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+    {
+        _source = source;
+        _selector = selector;
+    }
+
+    public IEnumerator<TResult> GetEnumerator()
+    {
+        if (_threadId == Environment.CurrentManagedThreadId && _state == 0)
+        {
+            _state = 1;
+            return this;
+        }
+
+        return new SelectManualEnumerable<TSource, TResult>(_source, _selector) { _state = 1 };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    
+    public bool MoveNext()
+    {
+        switch (_state)
+        {
+            case 1 :
+                _enumerator = _source.GetEnumerator();
+                _state = 2;
+                goto case 2;
+            case 2 :
+                Debug.Assert(_enumerator is not null);
+
+                try
+                {
+                    if (_enumerator.MoveNext())
+                    {
+                        _current = _selector(_enumerator.Current);
+                        return true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Dispose();
+                    throw;
+                }
+                break;
+        }
+        
+        Dispose();
+        return false;
+    }
+    
+    public void Dispose()
+    {
+        _state = -1;
+        _enumerator?.Dispose();
+    }
+
+    public void Reset()
+    {
+        throw new NotSupportedException();
+    }
+
+    public TResult Current => _current;
+
+    object IEnumerator.Current => Current;
+
 }
 
